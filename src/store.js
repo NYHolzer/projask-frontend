688ef70705f18233cb7f474e0735322553d74c4a
NYHolzer <nissan.holzer@gmail.com>
2019-11-16 21:50:42 -0500

    added loginForm to store

--- src/store.js
+++ src/store.js
@@ -2,10 +2,12 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import usersReducer from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
+import loginForm from './reducers/loginForm.js'
 
 const reducer = combineReducers({
     users: usersReducer,
-    currentUser
+    currentUser,
+    loginForm
 })
 
 const composeEnhancers = typeof window === 'object' &&
