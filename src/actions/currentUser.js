44acaadc3c8655dc2d7f3865369c67ad56266040
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 11:10:44 -0500

    currentUser actions udpated for login and getCurrentuser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -7,15 +7,49 @@ export const setCurrentUser = user => {
 }
 
 
-
 // asynchronous action creators
 export const login = credentials => {
-    return fetch("http://localhost:3001/api/v1/login",{
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(credentials)
-    })
-
+    console.log("credentials are", credentials) 
+    return dispatch => {
+        return fetch ("http://localhost:3001/api/v1/login",{
+            credentials: "include",
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
+        .then(r => r.json())
+        .then(user => {
+            if (user.error){
+                alert(user.error)
+            } else {
+                dispatch(setCurrentUser(user))
+            }
+        })
+        .catch(console.log)
+    }
 }
+
+
+  export const getCurrentUser = () => {
+    console.log("DISPATCHING GET CURRENT USER")
+    return dispatch => {
+      return fetch("http://localhost:3001/api/v1/get_current_user", {
+        credentials: "include",
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+        .then(r => r.json())
+        .then(user => {
+          if (user.error) {
+            alert(user.error)
+          } else {
+            dispatch(setCurrentUser(user))
+          }
+        })
+        .catch(console.log)
+    }
+  }
