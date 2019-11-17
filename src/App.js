dd55afb6ee341818c37b374f7c07009cc00daec4
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 11:09:49 -0500

    added login component with conenctor for getCurrentUser

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/Login.js'
+import { connect } from 'react-redux'
+import { getCurrentUser } from './actions/currentUser.js'
 
-function App() {
+class App extends React.Component {
 
-  return (
-    "hello I'm React"
-  );
+  componentDidMount(){
+    this.props.getCurrentUser()
+  }
+
+  render(){
+    return (
+      <Login />
+    );
+  }
+
 }
 
-export default App;
+export default connect(null, { getCurrentUser })(App);
