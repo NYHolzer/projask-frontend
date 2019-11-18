bc064742904645e72db9c1edcf587fbedde7711d
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 19:18:10 -0500

    create reducer case for clearing currentuser

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -48,24 +48,24 @@ export const logout = () => {
     }
 }
 
-  export const getCurrentUser = () => {
-    console.log("DISPATCHING GET CURRENT USER")
-    return dispatch => {
-      return fetch("http://localhost:3001/api/v1/get_current_user", {
-        credentials: "include",
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json"
-        },
-      })
-        .then(r => r.json())
-        .then(user => {
-          if (user.error) {
-            alert(user.error)
-          } else {
-            dispatch(setCurrentUser(user))
-          }
-        })
-        .catch(console.log)
-    }
-  }
+export const getCurrentUser = () => {
+console.log("DISPATCHING GET CURRENT USER")
+return dispatch => {
+    return fetch("http://localhost:3001/api/v1/get_current_user", {
+    credentials: "include",
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    })
+    .then(r => r.json())
+    .then(user => {
+        if (user.error) {
+        alert(user.error)
+        } else {
+        dispatch(setCurrentUser(user))
+        }
+    })
+    .catch(console.log)
+}
+}
--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -2,7 +2,11 @@ export default (state = null, action) => {
     switch(action.type){
         case "SET_CURRENT_USER":
             return action.user
+        case "CLEAR_CURRENT_USER":
+            return null 
         default:
             return state
     }
 }
+
+// Using null because return falsy and will easily be able to see if user
