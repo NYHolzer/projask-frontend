1983bf8e506ab1bbcca7b17fedf8dce862c7185e
NYHolzer <nissan.holzer@gmail.com>
2019-11-17 20:10:07 -0500

    added MainContainer

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const MainContainer = () => {
+    return(
+        <div className="MainContainer">
+            <p>THIS IS THE MAIN CONTAINER</p>
+        </div>
+    )
+}
+
+export default MainContainer
