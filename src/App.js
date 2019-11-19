48ad148c1827f6d410b3de8017f0d1ee8a9e232d
NYHolzer <nissan.holzer@gmail.com>
2019-11-18 21:25:00 -0500

    added routes for login, logout, and MyTasks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser.js'
 import NavBar from './components/NavBar'
 import MainContainer from './components/MainContainer'
+import MyTasks from './components/MyTasks.js'
+import Login from './components/Login.js'
+import Logout from './components/Logout.js'
 
 class App extends React.Component {
 
@@ -17,6 +21,9 @@ class App extends React.Component {
       <div className="App">
         <NavBar />
         <MainContainer />
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/logout' component={Logout}/>
+          <Route exact path='/tasks' component={MyTasks}/>
       </div>
     );
   }
