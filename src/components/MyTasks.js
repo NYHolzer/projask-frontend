ca98aaf2ae9ce6aa5357cd0dac3ede4ccdfb1757
NYHolzer <nissan.holzer@gmail.com>
2019-11-18 20:44:50 -0500

    create MyTasks that iterate the data over the TaskCards format

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -1,3 +1,14 @@
 import React from 'react'
 import TaskCard from './TaskCard.js'
 import { connect } from 'react-redux'
+
+
+const MyTasks = props => {
+    const taskCards = props.tasks.length > 0 ? 
+        props.tasks.map(t => <TaskCard task={t} key={t.id}/>) : 
+        null
+    return (
+        taskCards
+    )
+}
+
