fc786b274b44b4e75af65d4a6eedf90290844ca7
NYHolzer <nissan.holzer@gmail.com>
2019-11-18 23:01:45 -0500

    added signupForm reducers with initialState

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -1,5 +1,16 @@
-export default (state=[], action) => {
+const initialState = {
+    name: "",
+    username: "",
+    password: "",
+    work_email: ""
+}
+
+export default (state=initialState, action) => {
     switch (action.type){
+        case "UPDATE_SIGNUP_FORM":
+            return action.formData
+        case "RESET_SIGNUP_FORM":
+            return initialState
         default:
             return state
     }
