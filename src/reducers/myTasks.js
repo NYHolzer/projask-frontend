8f068739740a09d947a58cef7bffcc5a933411cf
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 17:42:56 -0500

    added clear current tasks reducer

--- src/reducers/myTasks.js
+++ src/reducers/myTasks.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type){
         case "SET_MY_TASKS":
             return action.tasks
+        case "CLEAR_CURRENT_TASKS":
+            return [] 
         default:
             return state
     }
