5ab4fbb2c8f9ba73ae2b828501ef23de339a00af
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 23:35:20 -0500

    add task reducer to complement action

--- src/reducers/myTasks.js
+++ src/reducers/myTasks.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
     switch(action.type){
         case "SET_MY_TASKS":
             return action.tasks
+        case "ADD_TASK":
+            return state.tasks.concat(action.tasks)
         case "CLEAR_CURRENT_TASKS":
             return [] 
         default:
