106ab3dc8c51b6cb721c09e124f913b6869c3dcd
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 20:52:30 -0500

    created NewTaskForm component

---
+++ src/components/NewTaskForm.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { updateNewTaskForm } from '../actions/newTaskForm.js'
+import { connect } from 'react-redux'
+
+const NewTaskForm = ({title, description, assigned_by, project_id, history, updateNewTaskForm}) => {
+
+    const formData = { title, description, assigned_by, project_id }
+
+    const handleChange = event => {
+        const { name, value } = event.target
+        updateNewTaskForm( name, value )
+    }
+    
+    const handleSubmit = event => {
+        event.preventDefault()
+
+    }
+
+    return(
+        <form>
+            <input 
+                placeholder = "title"
+                name="title"
+                onChange={handleChange}
+                value={title}
+            />
+            <input 
+                placeholder = "description"
+                name="description"
+                onChange={handleChange}
+                value={description}
+            />
+            <select value={assigned_by} onChange={handleChange}>
+                <option value="grapefruit">Grapefruit</option>
+            </select>
+            <input 
+                placeholder="assigned by"
+                name="assigned_by"
+                onChange={handleChange}
+                value={assigned_by}
+            />
+            <input 
+                placeholder= "project name"
+                name="project_id"
+                onChange={handleChange}
+                value={project_id} 
+            />
+            <input type="submit" value="Create Task"/>
+        </form>
+    )
+    
+
+}
+
+const mapStateToProps = state => {
+    return {
+        formData: state.newTaskForm
+    }
+}
+
+export default connect (mapStateToProps, {updateNewTaskForm}) (NewTaskForm);
+
+// Tasks Schema
+// t.string "title"
+// t.string "description"
+// t.string "assigned_by"
+// t.string "status_id"
+// t.string "dependent_task_id"
+// t.string "project_id"
