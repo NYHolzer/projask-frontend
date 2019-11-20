ddaae6420ea55a2b67341af04910bd8db339df8c
NYHolzer <nissan.holzer@gmail.com>
2019-11-19 20:51:48 -0500

    fixed up newTaskForm reducer a bit

--- src/reducers/newTaskForm.js
+++ src/reducers/newTaskForm.js
@@ -2,16 +2,17 @@ const initialState = {
     title: "",
     description: "",
     assigned_by: "",
-    project_id: 1
+    project_id: ""
 }
 
-export default (state = [], action) => {
+export default (state = initialState, action) => {
     switch(action.type){
         case "UPDATE_NEW_TASK_FORM":
-            return {
+            const returnVal = {
                 ...state,
                 [action.formData.name]: action.formData.value
             }
+            return returnVal
         case "RESET_NEW_TASK_FORM":
             return initialState
         default:
