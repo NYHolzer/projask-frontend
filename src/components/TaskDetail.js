8251e4e9a126f28ceecf95fa079365dc6a24ae61
NYHolzer <nissan.holzer@gmail.com>
2019-11-20 22:41:27 -0500

    added link on TaskDetail page to edit

--- src/components/TaskDetail.js
+++ src/components/TaskDetail.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const TaskDetail = ({ task }) => {
     return (
@@ -10,6 +11,7 @@ const TaskDetail = ({ task }) => {
             <p> {task.description}</p>
 
             <label> Assigned_by: </label> {task.assigned_by ? task.assigned_by : "Myself"}
+            <h4> <Link to={`/tasks/${task.id}/edit`}> Edit </Link> </h4>
         </div> :
 
         <p> This is the trip Card with no Trip </p>
