5a951eb487e9a253692b53bd3e6aff11d76b59e3
NYHolzer <nissan.holzer@gmail.com>
2019-11-20 22:31:39 -0500

    just removed Link that I thought was gonna be here but not anymore

--- src/components/MyTasks.js
+++ src/components/MyTasks.js
@@ -2,7 +2,6 @@ import React from 'react'
 import TaskCard from './TaskCard.js'
 import { connect } from 'react-redux'
 
-
 const MyTasks = props => {
     const taskCards = props.tasks.length > 0 ? 
         props.tasks.map(t => <TaskCard task={t} key={t.id}/>) : 
