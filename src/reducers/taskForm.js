0d8b7957c9be0d35da5cff3228be6e1aa266cd8b
NYHolzer <nissan.holzer@gmail.com>
2019-11-21 01:43:55 -0500

    set form data for edit reducer

--- src/reducers/taskForm.js
+++ src/reducers/taskForm.js
@@ -15,6 +15,8 @@ export default (state = initialState, action) => {
             return returnVal
         case "RESET_NEW_TASK_FORM":
             return initialState
+        case "SET_FORM_DATA_FOR_EDIT":
+            return action.taskFormData
         default:
             return state
     }
