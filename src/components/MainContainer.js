51d4deede6c2147f60b2d4fe5a786b45dd8d9448
NYHolzer <nissan.holzer@gmail.com>
2019-12-10 21:57:48 -0500

    added sorting functionality to Main Container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import MyTasks from './MyTasks.js'
 
-const MainContainer = () => {
-    return(
-        <div className="MainContainer">
-            <MyTasks/>
-        </div>
-    )
+
+class MainContainer extends React.Component {
+
+    constructor (props) {
+        super(props)
+        this.state = { pressed: false}
+    }
+
+    sortTaskByName = () => {
+        this.setState({
+            pressed: true
+        });
+    }
+
+    sortTaskByName(){
+        this.setState({
+            pressed: true
+        });
+    }
+
+    render(){
+        return(
+            <div className="MainContainer">
+                <button onClick={this.sortTaskByName.bind(this)} id="sortTaskByName"> SortTaskByName </button>
+                <MyTasks pressed={this.state.pressed}/>
+            </div>
+        )
+    }
+
 }
+
 
 export default MainContainer
